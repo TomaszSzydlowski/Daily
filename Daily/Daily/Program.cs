63591dc49a9ba2a00bbae1d17f0c0a18730eb512
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2020-01-23 21:07:11 +0100

    Added xDoc

--- Daily/Daily/Program.cs
+++ Daily/Daily/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Xml.Linq;
 
 namespace Daily
 {
@@ -8,8 +6,17 @@ namespace Daily
     {
         static void Main(string[] args)
         {
-        
 
+            XDocument xDoc = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XElement("Root",
+                new XElement("Task",
+                new XAttribute("Data", "data"), "tresc zdania")
+                )
+                );
+
+
+            xDoc.Save(@"C:\Home\Repo\01_Repository\31_Daily\Daily\Daily\Daily\XMLDoc.xml");
         }
     }
 
